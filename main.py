6de88ee1bce4151eaf9f783c076e0cbf818a9a23
Matias Debarbora <matias.debarbora@hotmail.com>
2025-01-29 14:52:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from colorama import init, Fore, Back, Style
+from tabulate import tabulate
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 import matplotlib.pyplot as plt
@@ -6,15 +8,23 @@ import matplotlib.pyplot as plt
 def main():
     api_key = "c86b3c8c49a759cd21dbe280885047d9"
     city = "Santiago, CL"
+    data = weather_json_data(api_key, city)
+    forecast_data = forecast_json_data(api_key, city)
 
-    fetch_weather(api_key, city)
+    fetch_weather(data, city)
 
-    forecast_list = fetch_tomorrow_forecast(api_key, city)
+    forecasts = fetch_tomorrow_forecast(forecast_data)
 
-    time_list = extract_time_list(forecast_list)
-    temp_list = extract_temp_list(forecast_list)
-    tomorrow_min_max(temp_list)
-    plot_tomorrow_temp(temp_list, time_list)
+    tomorrow_min_max(forecasts, 1)
+    tomorrow_min_max(forecasts, 2)
+    tomorrow_min_max(forecasts, 3)
+    tomorrow_min_max(forecasts, 4)
+    tomorrow_min_max(forecasts, 5)
+    print(f"{Fore.WHITE}")
+
+    # print(tabulate(forecast_list))
+    # time_list = extract_time_list(forecast_list)
+    # plot_tomorrow_temp(temp_list, time_list)
 
 
 def rounded_int(n):
@@ -22,7 +32,7 @@ def rounded_int(n):
     return rounded_number
 
 
-def fetch_weather(api_key, city):
+def weather_json_data(api_key, city):
     base_url = "https://api.openweathermap.org/data/2.5/weather"
     params = {
         "q": city,
@@ -33,30 +43,7 @@ def fetch_weather(api_key, city):
         response = requests.get(base_url, params=params)
         response.raise_for_status()
         data = response.json()
-
-        weather_description = data["weather"][0]["description"]
-        temperature = rounded_int(data["main"]["temp"])
-        humidity = data["main"]["humidity"]
-        wind_speed = data["wind"]["speed"]
-        
-        sunrise = data["sys"]["sunrise"]
-        sunrise_time = datetime.fromtimestamp(sunrise, ZoneInfo("utc"))
-        local_timezone = ZoneInfo("America/Santiago")
-        local_sunrise = sunrise_time.astimezone(local_timezone)
-        formatted_sunrise = local_sunrise.strftime("%H:%M:%S")
-        #formatted_sunrise = local_sunrise.strftime("%Y-%m-%d %H:%M:%S")
-
-        sunset = data["sys"]["sunset"]
-        sunset_time = datetime.fromtimestamp(sunset, ZoneInfo("utc"))
-        local_sunset = sunset_time.astimezone(local_timezone)
-        formatted_sunset = local_sunset.strftime("%H:%M:%S")
-        #formatted_sunrise = local_sunrise.strftime("%Y-%m-%d %H:%M:%S")
-        
-        print("")
-        print(f"Weather in {city.capitalize()}:")
-        print(f"{weather_description.capitalize()} - Temp: {temperature}°C - Hum: {humidity}% - Wind Speed: {wind_speed} m/s")
-        print(f"Sunrise: {formatted_sunrise} - Sunset: {formatted_sunset}")
-        print("")
+        return data
 
     except requests.exceptions.RequestException as e:
         print(f"Error fetching weather data: {e}")
@@ -64,46 +51,78 @@ def fetch_weather(api_key, city):
         print("Error: Unexpected response format. Please check the city name or API key.")
 
 
-def fetch_tomorrow_forecast(api_key, city):
+def forecast_json_data(api_key, city):
     base_url = "https://api.openweathermap.org/data/2.5/forecast"
     params = {
         "q": city,
         "appid": api_key,
-        "units": "metric"
+        "units": "metric" 
     }
     try:
         response = requests.get(base_url, params=params)
         response.raise_for_status()
         data = response.json()
-
-        tomorrow = datetime.now() + timedelta(days=1)
-        tomorrow_date = tomorrow.date()
-
-        forecasts = data["list"]
-        forecast_list = []
-        
-        for forecast in forecasts:
-            forecast_dict = {}
-            forecast_time = datetime.fromtimestamp(forecast["dt"])
-            if forecast_time.date() == tomorrow_date:
-                time = forecast_time.strftime("%H:%M")
-                description = forecast["weather"][0]["description"]
-                temp = forecast["main"]["temp"]
-                humidity = forecast["main"]["humidity"]
-                wind_speed = forecast["wind"]["speed"]
-
-                forecast_dict["Time"] = time
-                forecast_dict["Description"] = description
-                forecast_dict["Temp"] = temp
-                forecast_dict["Hum"] = humidity
-                forecast_dict["Wind_speed"] = wind_speed
-                forecast_list.append(forecast_dict)
-        return forecast_list
+        return data
 
     except requests.exceptions.RequestException as e:
         print(f"Error fetching weather data: {e}")
     except KeyError:
         print("Error: Unexpected response format. Please check the city name or API key.")
+
+
+def fetch_weather(data, city):
+    weather_description = data["weather"][0]["description"]
+    temperature = rounded_int(data["main"]["temp"])
+    humidity = data["main"]["humidity"]
+    wind_speed = data["wind"]["speed"]
+    
+    sunrise = data["sys"]["sunrise"]
+    sunrise_time = datetime.fromtimestamp(sunrise, ZoneInfo("utc"))
+    local_timezone = ZoneInfo("America/Santiago")
+    local_sunrise = sunrise_time.astimezone(local_timezone)
+    formatted_sunrise = local_sunrise.strftime("%H:%M:%S")
+    #formatted_sunrise = local_sunrise.strftime("%Y-%m-%d %H:%M:%S")
+
+    sunset = data["sys"]["sunset"]
+    sunset_time = datetime.fromtimestamp(sunset, ZoneInfo("utc"))
+    local_sunset = sunset_time.astimezone(local_timezone)
+    formatted_sunset = local_sunset.strftime("%H:%M:%S")
+    #formatted_sunrise = local_sunrise.strftime("%Y-%m-%d %H:%M:%S")
+    
+    print("")
+    print(f"{Fore.GREEN}Weather in {city.capitalize()}:")
+    print(f"{weather_description.capitalize()} - Temp: {temperature}°C - Hum: {humidity}% - Wind Speed: {wind_speed} m/s")
+    print(f"Sunrise: {formatted_sunrise} - Sunset: {formatted_sunset}{Fore.YELLOW}")
+    print("")
+
+
+def fetch_tomorrow_forecast(data):
+    forecasts = data["list"]
+    return forecasts
+        
+
+def day_forecast_list(forecasts, day):
+    tomorrow = datetime.now() + timedelta(days=day)
+    tomorrow_date = tomorrow.date()
+    forecast_list = []
+
+    for forecast in forecasts:
+        forecast_dict = {}
+        forecast_time = datetime.fromtimestamp(forecast["dt"])
+        if forecast_time.date() == tomorrow_date:
+            time = forecast_time.strftime("%H:%M")
+            description = forecast["weather"][0]["description"]
+            temp = forecast["main"]["temp"]
+            humidity = forecast["main"]["humidity"]
+            wind_speed = forecast["wind"]["speed"]
+
+            forecast_dict["Time"] = time
+            forecast_dict["Description"] = description
+            forecast_dict["Temp"] = temp
+            forecast_dict["Hum"] = humidity
+            forecast_dict["Wind_speed"] = wind_speed
+            forecast_list.append(forecast_dict)
+    return forecast_list
 
 
 def extract_time_list(forecast_list):
@@ -122,15 +141,13 @@ def extract_temp_list(forecast_list):
     return temp_list
 
 
-def tomorrow_min_max(temp_list):
-    tomorrow = datetime.now() + timedelta(days=1)
+def tomorrow_min_max(forecasts, day):
+    temp_list = extract_temp_list(day_forecast_list(forecasts, day))
+    tomorrow = datetime.now() + timedelta(days=day)
     tomorrow_date = tomorrow.date()
-    print(f"Tomorrow's forecast {tomorrow_date}:")
-
     min_temp = rounded_int(min(temp_list))
     max_temp = rounded_int(max(temp_list))
-    print(f"Min temp: {min_temp}°c - Max temp: {max_temp}°c")
-    print("")
+    print(f"{tomorrow_date} --> Min temp: {min_temp}°c - Max temp: {max_temp}°c")
 
 
 def plot_tomorrow_temp(temp_list, time_list):
@@ -152,3 +169,4 @@ def plot_tomorrow_temp(temp_list, time_list):
 
 if __name__ == "__main__":
     main()
+    
